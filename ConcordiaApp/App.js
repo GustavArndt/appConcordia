0788f86fb2a305c4d04274438750f326a28d53b7
GustavArndt <gustavo.tycobb@gmail.com>
2021-04-12 19:58:50 -0300

    inicio

--- ConcordiaApp/App.js
+++ ConcordiaApp/App.js
@@ -1,1 +1,14 @@
 import React from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+
+
+
+export default function concordiaApp(){
+  return(
+
+      <View>
+        <Text>meu App</Text>
+      </View>
+    
+  )
+}
